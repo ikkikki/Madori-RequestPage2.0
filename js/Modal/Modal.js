bb2e4798fd259d72ce7e190d2ab1e331a02dad3b
sample name <sample@example.com>
2020-01-20 10:10:26 +0900

    Update Modal.js

--- js/Modal/Modal.js
+++ js/Modal/Modal.js
@@ -5,9 +5,9 @@ console.log('Modal running..')
 
 
 
-// *** DOM methods
-function ModalDOM(){
-  let m = $('#modal');
-  m.append($('<div class="modalwindow"></div>'))
-  m.hide();
-}
+// // *** DOM methods
+// function ModalDOM(){
+//   let m = $('#modal');
+//   m.append($('<div class="modalwindow"></div>'))
+//   m.hide();
+// }
