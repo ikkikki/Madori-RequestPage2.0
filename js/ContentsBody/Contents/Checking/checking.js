c9fdc75b2f27d74a73586648a8a7f8c219bc1f8b
sample name <sample@example.com>
2020-01-25 12:41:39 +0900

    up

--- js/ContentsBody/Contents/Checking/checking.js
+++ js/ContentsBody/Contents/Checking/checking.js
@@ -6,31 +6,51 @@ console.log('checking running..')
 
 
 // ***** component
-async function OrderProps(){
-  OrderDOM();
-  let acceptable = sessionStorage.getItem('acceptable');
-  let drawings = JSON.parse(sessionStorage.getItem('drawings'));
-  let requested = sessionStorage.getItem('requested');
-  // console.log(drawings.hasOwnProperty('status'));
-  if (Object.keys(drawings).length){
-    orderImgDOM(drawings);
-  }
-  if (acceptable==='true'&&requested==='true'){
-    orderButtonDOM();
-  }
+async function CheckingProps(){
+  CheckingDOM();
 }
 
 
 // *** DOM methods
+function CheckingDOM(){
+  let DOM = $('.checking');
+  let basediv = $('<div class="checkingContents"></div>');
+  DOM.html(basediv);
+}
 
+function checkingImgDOM(drawings){
+  console.log('checkingImgDOM running..')
+  let DOM = $('.checkingContents');
+  let gallary = $('<ul class="checkedImages"></ul>');
+  for (let key in drawings){
+    dic = drawings[key];
+    title = document.createElement('p');
+    title.innerText = '物件番号　：'+key;
+    period = document.createElement('p');
+    period.innerText = '作図納期　：'+dic['period'];
+    li = document.createElement('li');
+    li.classList.add('ordercard');
+    orimg = document.createElement('img');
+    orimg.src = dic['requestjpg'];
+    primg = document.createElement('img');
+    primg.src = dic['printedjpg'];
+    li.append(title);
+    li.append(period);
+    li.append(orimg);
+    li.append(primg);
+    gallary.append(li);
+  }
+  DOM.append(gallary);
+}
 
 // ***  drive methods
+
+
 
 // *  modal action
 
 
 // ***  API call (put)
-f
 
 
 
